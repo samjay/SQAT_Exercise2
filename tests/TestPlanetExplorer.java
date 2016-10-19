2fd5950a8f6b85d24e3fd64a32fe4f209fcb6c16
somename <someemail>
2016-10-19 15:28:55 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -113,7 +113,7 @@ public class TestPlanetExplorer {
 	
 	@Test
 	public void test_executeCommand_wrap_moveForward_990Nto00N() throws PlanetExplorerException{	
-		for(int i=0;i<100;i++){
+		for(int i=0;i<99;i++){
 			explorer.executeCommand("f");
 		}
 		String position=explorer.executeCommand("f");

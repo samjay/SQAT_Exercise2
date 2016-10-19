9496b05699e6b13b9cf441f55dba7f22c9b49f96
somename <someemail>
2016-10-19 14:34:57 +0300

    besouro automatic message

--- src/Planet.java
+++ src/Planet.java
@@ -1,4 +1,19 @@
 
 public class Planet {
+	
+	private int width;
+	private int length;
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public int getLength() {
+		return length;
+	}
+	public void setLength(int length) {
+		this.length = length;
+	}	
 
 }

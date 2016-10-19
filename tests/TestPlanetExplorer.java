8ca491f2ec05ce249c2e67023f4c510768211388
somename <someemail>
2016-10-19 14:34:57 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -6,7 +6,7 @@ public class TestPlanetExplorer {
 
 	@Test
 	public void test_createPlanet_size100x100_width100() {
-		PlanetExplorer explorer= new PlanetExplorer(100, 100, 0);
+		PlanetExplorer explorer= new PlanetExplorer(100, 100, "");
 		Planet planet = explorer.getPlanet();
 		
 		assertEquals(100, planet.getWidth());

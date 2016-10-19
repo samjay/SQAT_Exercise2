4441a551b0aeb5f90482a2fd2994b904ffd1f28b
somename <someemail>
2016-10-19 14:37:58 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -4,6 +4,9 @@
 // Finish time:
 
 public class PlanetExplorer {
+	
+	Planet planet;
+	
 	public PlanetExplorer(int x, int y, String obstacles){
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
@@ -12,6 +15,10 @@ public class PlanetExplorer {
 		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)")  
 		 
 	 */
+		planet= new Planet();
+		planet.setLength(x);
+		planet.setWidth(y);
+		
 	}
 	
 	public String executeCommand(String command){
@@ -31,6 +38,6 @@ public class PlanetExplorer {
 
 	public Planet getPlanet() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.planet;
 	}
 }
--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -5,10 +5,11 @@ import org.junit.Test;
 public class TestPlanetExplorer {
 
 	@Test
-	public void test_createPlanet_size100x100_width100() {
+	public void test_createPlanet_size100x100() {
 		PlanetExplorer explorer= new PlanetExplorer(100, 100, "");
 		Planet planet = explorer.getPlanet();
 		
 		assertEquals(100, planet.getWidth());
+		assertEquals(100, planet.getLength());
 	}
 }

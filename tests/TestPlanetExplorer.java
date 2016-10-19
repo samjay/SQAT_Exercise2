19be04f96f4e85dccb9a9e5a29f9f68bb5fd6033
somename <someemail>
2016-10-19 14:41:52 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -12,4 +12,13 @@ public class TestPlanetExplorer {
 		assertEquals(100, planet.getWidth());
 		assertEquals(100, planet.getLength());
 	}
+	
+	@Test
+	public void test_executeCommand_atStart_explorer_00N(){
+		PlanetExplorer explorer= new PlanetExplorer(100, 100, "");
+		
+		String position=explorer.executeCommand("");
+		
+		assertEquals("(0,0,N)", position);
+	}
 }

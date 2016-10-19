447941e71e44c180436ad929a4d45dc9c1893861
somename <someemail>
2016-10-19 15:40:50 +0300

    besouro automatic message

--- src/Obstacle.java
+++ src/Obstacle.java
@@ -1,4 +1,20 @@
 
 public class Obstacle {
 
+	int x;
+	int y;
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	
 }
